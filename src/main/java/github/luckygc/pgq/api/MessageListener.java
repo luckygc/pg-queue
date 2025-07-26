a85444d010e6d949ec10f69a5114e9681032351d
luckygc <chenwwecm@gmail.com>
2025-07-26 23:06:21 +0800

    重构消息监听器相关

--- src/main/java/github/luckygc/pgq/api/MessageListener.java
+++ src/main/java/github/luckygc/pgq/api/MessageListener.java
@@ -2,8 +2,6 @@ package github.luckygc.pgq.api;
 
 public interface MessageListener {
 
-    String topic();
-
     /**
      * 不允许阻塞
      */
---
+++ src/main/java/github/luckygc/pgq/support/SingleMessageProcessor.java
@@ -0,0 +1,68 @@
+package github.luckygc.pgq.support;
+
+import github.luckygc.pgq.Message;
+import github.luckygc.pgq.api.MessageListener;
+import github.luckygc.pgq.api.PgQueue;
+import github.luckygc.pgq.api.SingleMessageHandler;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Semaphore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SingleMessageProcessor implements MessageListener {
+
+    private static final Logger log = LoggerFactory.getLogger(SingleMessageProcessor.class);
+    private final Semaphore semaphore;
+    private final int pullCount;
+    private final PgQueue pgQueue;
+    private final SingleMessageHandler messageHandler;
+
+    public SingleMessageProcessor(PgQueue pgQueue, SingleMessageHandler messageHandler, int pullCount,
+            int threadCount) {
+        this.pgQueue = Objects.requireNonNull(pgQueue);
+        this.messageHandler = Objects.requireNonNull(messageHandler);
+        if (pullCount < 1) {
+            throw new IllegalArgumentException("pullCount必须大于0");
+        }
+        this.pullCount = pullCount;
+
+        if (threadCount < 1) {
+            throw new IllegalArgumentException("threadCount必须大于0");
+        }
+        this.semaphore = new Semaphore(threadCount);
+    }
+
+    @Override
+    public void onMessageAvailable() {
+        if (!semaphore.tryAcquire()) {
+            return;
+        }
+
+        Runnable r = () -> {
+            try {
+                List<Message> messages;
+                while (!(messages = pgQueue.pull(pullCount)).isEmpty()) {
+                    for (Message message : messages) {
+                        try {
+                            messageHandler.handle(message);
+                        } catch (Throwable t) {
+                            log.error("处理消息失败", t);
+                        }
+                    }
+                }
+            } finally {
+                semaphore.release();
+            }
+        };
+
+        try {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            thread.start();
+        } catch (Throwable t) {
+            semaphore.release();
+            log.error("启动消息处理线程失败", t);
+        }
+    }
+}
