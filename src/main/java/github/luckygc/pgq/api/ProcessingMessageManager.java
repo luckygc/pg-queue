3eee2b1b6318f23a8832d86b8a54bc945b406f66
luckygc <chenwwecm@gmail.com>
2025-07-26 22:18:00 +0800

    重构MessageManager为ProcessingMessageManager

---
+++ src/main/java/github/luckygc/pgq/api/ProcessingMessageManager.java
@@ -0,0 +1,25 @@
+package github.luckygc.pgq.api;
+
+import github.luckygc.pgq.Message;
+import java.time.Duration;
+import java.util.List;
+import org.jspecify.annotations.Nullable;
+
+public interface ProcessingMessageManager {
+
+    void complete(Message message);
+
+    void complete(List<Message> messages);
+
+    void dead(Message message);
+
+    void dead(List<Message> messages);
+
+    void delete(Message message);
+
+    void delete(List<Message> messages);
+
+    void retry(Message message, @Nullable Duration processDelay);
+
+    void retry(List<Message> messages, @Nullable Duration processDelay);
+}
