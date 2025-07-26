e2f85251ffcc9c7ed418061b4baf10815b023c41
luckygc <chenwwecm@gmail.com>
2025-07-26 15:42:11 +0800

    添加builder

---
+++ src/main/java/github/luckygc/pgq/PgqBuilder.java
@@ -0,0 +1,15 @@
+package github.luckygc.pgq;
+
+import github.luckygc.pgq.api.MessageProcessor;
+import github.luckygc.pgq.api.PgQueue;
+
+public class PgqBuilder {
+
+    private String topic;
+    private MessageProcessor messageProcessor;
+
+
+    public PgQueue build() {
+        return null;
+    }
+}
