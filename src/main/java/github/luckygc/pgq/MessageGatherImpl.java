1990e33c1a619a2bb0336bfeed524ac789d2e59d
luckygc <chenwwecm@gmail.com>
2025-07-26 16:00:34 +0800

    添加MessageGather实现类

---
+++ src/main/java/github/luckygc/pgq/MessageGatherImpl.java
@@ -0,0 +1,22 @@
+package github.luckygc.pgq;
+
+import github.luckygc.pgq.api.MessageGather;
+import java.time.Duration;
+
+public class MessageGatherImpl implements MessageGather {
+
+    @Override
+    public MessageGather priority(int priority) {
+        return null;
+    }
+
+    @Override
+    public MessageGather processDelay(Duration delay) {
+        return null;
+    }
+
+    @Override
+    public void push() {
+
+    }
+}
