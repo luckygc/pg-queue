3878a62e929f566ec10f5634c54c7f1bc899a95e
luckygc <chenwwecm@gmail.com>
2025-07-26 17:18:49 +0800

    添加nullable注解

--- src/main/java/github/luckygc/pgq/api/MessageGather.java
+++ src/main/java/github/luckygc/pgq/api/MessageGather.java
@@ -1,12 +1,13 @@
 package github.luckygc.pgq.api;
 
 import java.time.Duration;
+import org.jspecify.annotations.Nullable;
 
 public interface MessageGather {
 
     MessageGather priority(int priority);
 
-    MessageGather processDelay(Duration delay);
+    MessageGather processDelay(@Nullable Duration delay);
 
     void push();
 }
--- src/main/java/github/luckygc/pgq/api/MessageManager.java
+++ src/main/java/github/luckygc/pgq/api/MessageManager.java
@@ -3,6 +3,7 @@ package github.luckygc.pgq.api;
 import github.luckygc.pgq.Message;
 import java.time.Duration;
 import java.util.List;
+import org.jspecify.annotations.Nullable;
 
 public interface MessageManager {
 
@@ -10,7 +11,7 @@ public interface MessageManager {
 
     void complete(List<Message> messages, boolean delete);
 
-    void retry(Message message, Duration processDelay);
+    void retry(Message message, @Nullable Duration processDelay);
 
     void retry(List<Message> messages, Duration processDelay);
 
