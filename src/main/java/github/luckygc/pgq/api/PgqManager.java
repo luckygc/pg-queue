01c1b6a70e359a0870df8f322f964c134fad50d3
luckygc <chenwwecm@gmail.com>
2025-07-27 01:17:33 +0800

    调整接口

--- src/main/java/github/luckygc/pgq/api/PgqManager.java
+++ src/main/java/github/luckygc/pgq/api/PgqManager.java
@@ -1,12 +1,13 @@
 package github.luckygc.pgq.api;
 
-import github.luckygc.pgq.QueueDao;
 import java.sql.SQLException;
 import org.jspecify.annotations.Nullable;
 
 public interface PgqManager {
 
     PgQueue registerQueue(String topic);
+
+    PgQueue registerQueue(String topic, MessageListener messageListener);
 
     @Nullable
     PgQueue getQueue(String topic);
