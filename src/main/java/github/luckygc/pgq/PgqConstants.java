e8309afe7c1a43edcedf64b6547e143b146d4256
luckygc <chenwwecm@gmail.com>
2025-07-26 22:32:22 +0800

    添加常量

--- src/main/java/github/luckygc/pgq/PgqConstants.java
+++ src/main/java/github/luckygc/pgq/PgqConstants.java
@@ -4,7 +4,19 @@ import java.time.Duration;
 
 public class PgqConstants {
 
-    public static final int DEFAULT_PRIORITY = 0;
+    /**
+     * 默认消息优先级
+     */
+    public static final int MESSAGE_PRIORITY = 0;
 
+    /**
+     * 默认消息处理超时时间
+     */
     public static final Duration PROCESS_TIMEOUT = Duration.ofMinutes(10);
+
+    /*--------------------------------sql start----------------------------------*/
+    private static final int PGQ_ID = 199738;
+    private static final int SCHEDULER_ID = 1;
+    private static final String CHANNEL_NAME = "pgq_topic_channel";
+    /*--------------------------------sql end------------------------------------*/
 }
