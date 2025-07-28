f0ce8ece90b97d8afb8e143f5429501babc4bd34
luckygc <chenwwecm@gmail.com>
2025-07-29 00:12:55 +0800

    修复ddl

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,11 +1,11 @@
-create sequence if not exists pgq_message_seq start
+create sequence if not exists pgmq_message_seq start
     with
     1 increment by 1 minvalue 1;
 
-drop table if exists pgq_invisible_queue;
-create table pgq_invisible_queue
+drop table if exists pgmq_invisible_queue;
+create table pgmq_invisible_queue
 (
-    id           bigint                            default nextval('pgq_message_seq') primary key,
+    id           bigint                            default nextval('pgmq_message_seq') primary key,
     create_time  timestamp                not null default now(),
     topic        varchar(100) collate "C" not null,
     priority     int                      not null default 0,
@@ -14,12 +14,12 @@ create table pgq_invisible_queue
     visible_time timestamp                not null
 );
 
-create index pgq_invisible_queue_visible_time on pgq_invisible_queue using btree (visible_time);
+create index pgmq_invisible_queue_visible_time on pgmq_invisible_queue using btree (visible_time);
 
-drop table if exists pgq_pending_queue;
-create table pgq_pending_queue
+drop table if exists pgmq_pending_queue;
+create table pgmq_pending_queue
 (
-    id          bigint                            default nextval('pgq_message_seq') primary key,
+    id          bigint                            default nextval('pgmq_message_seq') primary key,
     create_time timestamp                not null default now(),
     topic       varchar(100) collate "C" not null,
     priority    int                      not null default 0,
@@ -27,14 +27,14 @@ create table pgq_pending_queue
     attempt     int                      not null default 0
 );
 
-create index idx_pgq_pending_queue_topic_priority_id on pgq_pending_queue (
+create index idx_pgmq_pending_queue_topic_priority_id on pgmq_pending_queue (
                                                                            topic,
                                                                            priority desc,
                                                                            id asc
     );
 
-drop table if exists pgq_processing_queue;
-create table pgq_processing_queue
+drop table if exists pgmq_processing_queue;
+create table pgmq_processing_queue
 (
     id           bigint primary key,
     create_time  timestamp                not null,
@@ -45,22 +45,10 @@ create table pgq_processing_queue
     timeout_time timestamp                not null
 );
 
-create index idx_pgq_processing_queue_timeout_time on pgq_processing_queue using btree (timeout_time);
+create index idx_pgmq_processing_queue_timeout_time on pgmq_processing_queue using btree (timeout_time);
 
-drop table if exists pgq_complete_queue;
-create table pgq_complete_queue
-(
-    id            bigint primary key,
-    create_time   timestamp                not null,
-    topic         varchar(100) collate "C" not null,
-    priority      int                      not null,
-    payload       varchar collate "C"      not null,
-    attempt       int                      not null,
-    complete_time timestamp                not null
-);
-
-drop table if exists pgq_dead_queue;
-create table pgq_dead_queue
+drop table if exists pgmq_dead_queue;
+create table pgmq_dead_queue
 (
     id          bigint primary key,
     create_time timestamp                not null,
@@ -71,4 +59,4 @@ create table pgq_dead_queue
     dead_time   timestamp                not null
 );
 
-create index idx_pgq_dead_queue_topic on pgq_dead_queue (topic);
+create index idx_pgmq_dead_queue_topic on pgmq_dead_queue (topic);
