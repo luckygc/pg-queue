4e1e2407dcd455defd13681384f893823c687d11
luckygc <chenwwecm@gmail.com>
2025-07-28 20:17:40 +0800

    重构

--- src/main/java/github/luckygc/pgq/api/manager/HandlerManager.java
+++ src/main/java/github/luckygc/pgq/api/manager/HandlerManager.java
@@ -1,5 +1,11 @@
 package github.luckygc.pgq.api.manager;
 
+import github.luckygc.pgq.api.handler.BatchMessageHandler;
+import github.luckygc.pgq.api.handler.SingleMessageHandler;
+
 public interface HandlerManager {
 
+    void registerMessageHandler(SingleMessageHandler messageHandler);
+
+    void registerMessageHandler(BatchMessageHandler messageHandler);
 }
