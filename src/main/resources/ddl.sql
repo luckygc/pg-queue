28bed54f1170dd1c7d6f23870951c91a9946da22
luckygc <chenwwecm@gmail.com>
2025-07-29 01:47:38 +0800

    添加pg函数

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -28,9 +28,9 @@ create table pgmq_pending_queue
 );
 
 create index idx_pgmq_pending_queue_topic_priority_id on pgmq_pending_queue (
-                                                                           topic,
-                                                                           priority desc,
-                                                                           id asc
+                                                                             topic,
+                                                                             priority desc,
+                                                                             id asc
     );
 
 drop table if exists pgmq_processing_queue;
@@ -60,3 +60,48 @@ create table pgmq_dead_queue
 );
 
 create index idx_pgmq_dead_queue_topic on pgmq_dead_queue (topic);
+
+
+drop function if exists pgmq_move_timeout_and_visible_msg_to_pending_then_notify;
+CREATE FUNCTION pgmq_move_timeout_and_visible_msg_to_pending_then_notify()
+    RETURNS TABLE
+            (
+                _topic varchar(100)
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NOT pg_try_advisory_xact_lock(1997, 38) THEN
+        RETURN;
+    END IF;
+
+    -- 如果拿到锁，则执行你的 delete/insert/notify 逻辑，并返回所有 topic
+    RETURN QUERY
+        WITH timeout_processing_messages AS (
+            DELETE FROM pgmq_processing_queue
+                WHERE timeout_time <= now()
+                RETURNING id, create_time, topic, priority, payload, attempt),
+             visible_messages AS (
+                 DELETE FROM pgmq_invisible_queue
+                     WHERE visible_time <= now()
+                     RETURNING id, create_time, topic, priority, payload, attempt),
+             message_to_pending AS (SELECT id, create_time, topic, priority, payload, attempt
+                                    FROM timeout_processing_messages
+                                    UNION ALL
+                                    SELECT id, create_time, topic, priority, payload, attempt
+                                    FROM visible_messages),
+             insert_op AS (
+                 INSERT INTO pgmq_pending_queue (id, create_time, topic, priority, payload, attempt)
+                     SELECT id, create_time, topic, priority, payload, attempt
+                     FROM message_to_pending),
+             message_available_topics AS (SELECT DISTINCT topic
+                                          FROM pgmq_pending_queue),
+             _notify AS (SELECT pg_notify('pgmq_topic_channel', topic), topic
+                         FROM message_available_topics)
+        SELECT topic
+        FROM _notify;
+END;
+$$;
+
+select pgmq_move_timeout_and_visible_msg_to_pending_then_notify()
